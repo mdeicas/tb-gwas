b5d568eed430d1e71b2eb01c069e3bde586d775f
Marco Deicas <mddeicas@gmail.com>
2022-12-17 19:51:27 -0500

    reorganize pyseer

--- pyseer/slurm_jobs/enet_model_job.sh
+++ pyseer/slurm_jobs/enet_model_job.sh
@@ -5,19 +5,16 @@
 
 phenotypes="/workdir/moldova/transmission/cluster/allclusterPheno_0.0005.txt" 
 vcf="/workdir/moldova/base_data/Moldova_SNPs_QC.vcf"
-distances="/workdir/mdd227/data/distances.tsv"
+distances="../distances.tsv"
 out="assoc.out"
 
 echo "Starting Job"
 source /programs/miniconda3/bin/activate pyseer	
 srun pyseer --wg enet --phenotypes $phenotypes --vcf $vcf --distances $distances --min-af 0.02 --max-af 0.98 > $out
-
 # sort by p-value to easily see significant SNPs
-source process_output.sh $out
-
+source ../analyze_data/process_output.sh $out
 # plot
-source plot.sh 
-python3 plot.py 
-
+source ../analyze_data/plot.sh 
+python3 ../analyze_data/plot.py 
 conda deactivate
 echo "Finished Job"
--- pyseer/slurm_jobs/fixed_effects_job.sh
+++ pyseer/slurm_jobs/fixed_effects_job.sh
@@ -4,20 +4,17 @@
 #SBATCH --time=60
 
 phenotypes="/workdir/moldova/transmission/cluster/allclusterPheno_0.0005.txt" 
-vcf="/workdir/moldova/base_data/Moldova_INDELs_QC.vcf"
-distances="/workdir/mdd227/data/distances.tsv"
+vcf="/workdir/moldova/base_data/Moldova_SNPs.vcf"
+distances="../distances.tsv"
 out="assoc.out"
 
 echo "Starting Job"
 source /programs/miniconda3/bin/activate pyseer	
 srun pyseer --phenotypes $phenotypes --vcf $vcf --distances $distances --min-af 0.02 --max-af 0.98 > $out
-
 # sort by p-value to easily see significant SNPs
-source process_output.sh $out
-
+source ../analyze_data/process_output.sh $out
 # plot
-source plot.sh 
-python3 plot.py 
-
+source ../analyze_data/plot.sh 
+python3 ../analyze_data/plot.py 
 conda deactivate
 echo "Finished Job"
